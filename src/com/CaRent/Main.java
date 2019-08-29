55e87e738dd1bf4faef668bb0785dafe5eea0eab
TheAbyssWalker <superrap4761@gmail.com>
2019-08-29 10:55:40 +0530

    first commit

---
+++ src/com/CaRent/Main.java
@@ -0,0 +1,26 @@
+package com.CaRent;
+
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1.Rent a Car\n2.Exit");
+        Scanner scan = new Scanner(System.in);
+        int ch = scan.nextInt();
+        if(ch == 2){
+            exit(0);
+        }
+
+        else{
+            RentalManager rm = new RentalManager();
+            rm.getDetails();
+            if(!rm.canRent()){
+                exit(0);
+            }
+        }
+    }
+}
