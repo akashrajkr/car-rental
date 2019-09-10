f5895cc57d2f95153f735bf6ecca4bcdc3ecbece
akashrajkr <aakashrajkr@gmail.com>
2019-09-10 18:10:32 +0530

    Age calculation made simple using LocalDate class

--- src/Driver.java
+++ src/Driver.java
@@ -1,14 +1,14 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Driver {
     private String driver_name, DOB, DIN, vtype, DLtype;
-    private Date dob;
 
     public void getDetails() {
         try {
@@ -18,8 +18,6 @@ public class Driver {
             driver_name = scan.nextLine();
             System.out.println("Enter your Date of Birth(dd/mm/yyyy)");
             DOB = scan.nextLine();
-            dob = new SimpleDateFormat("dd/MM/yyyy").parse(DOB);
-            System.out.println(dob);
             System.out.println("Enter your Driver Identification Number");
             DIN = scan.nextLine();
             System.out.println("What kind of licence do you own?\n1. Full\n2. Provisional");
@@ -33,7 +31,7 @@ public class Driver {
     }
 
     public boolean canRent() {
-        String[] dob_split = DOB.split("/");
+       /* String[] dob_split = DOB.split("/");
         int[] birthdate = new int[dob_split.length];
 
         for (int i = 0; i < birthdate.length; i++) {
@@ -43,14 +41,22 @@ public class Driver {
         int age = cal.getWeekYear() - birthdate[2];
 
         if (age == 17 || age == 21 || age == 25) {
-            if (cal.get(cal.MONTH) < birthdate[1])
+            if (cal.get(Calendar.MONTH) < birthdate[1])
                 age--;
-            else if (cal.get(cal.MONTH) == birthdate[1]) {
-                if (cal.get(cal.DATE) < birthdate[0])
+            else if (cal.get(Calendar.MONTH) == birthdate[1]) {
+                if (cal.get(Calendar.DATE) < birthdate[0])
                     age--;
             }
         }
-/*
+*/
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dob = LocalDate.parse(DOB, fmt);
+        LocalDate now = LocalDate.now();
+        Period diff = Period.between(dob, now);
+        int age = diff.getYears();
+//        System.out.println("Age = " + diff.getYears());
+        /*
+
     1. Small car
     2. Family car
     3. Luxury car
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -13,63 +13,68 @@ import java.util.concurrent.TimeUnit;
 
 class Vehicle {
     protected Date startDate, endDate;
-//    localDateTime objects for adding days to Date objects
+    //    localDateTime objects for adding days to Date objects
     protected LocalDate lStartDate, lEndDate;
     String date;
     protected int cost;
     protected long days;
     protected Period actualDays;
     protected LocalDate returnDate;
+
     public void rentNow() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Vehicle required from (press <Enter> to start from today)  (dd/mm/yyyy) : ");
         date = scan.nextLine();
-        if(date.isEmpty()) {
+        if (date.isEmpty()) {
             startDate = new Date();
             lStartDate = LocalDate.now();
         } else {
             try {
                 startDate = new SimpleDateFormat("dd/MM/yyyy").parse(date);
-                lStartDate = LocalDate.ofInstant(startDate.toInstant(),  ZoneId.systemDefault());
+                lStartDate = LocalDate.ofInstant(startDate.toInstant(), ZoneId.systemDefault());
             } catch (ParseException e) {
                 e.printStackTrace();
                 System.out.println("Parse error");
             }
         }
-        System.out.println("Enter your Date of Return(dd/mm/yyyy) : ");
-        date = scan.nextLine();
-        try {
-            endDate = new SimpleDateFormat("dd/MM/yyyy").parse(date);
-            lEndDate = LocalDate.ofInstant(endDate.toInstant(),  ZoneId.systemDefault());
-        } catch (ParseException e) {
-            e.printStackTrace();
+        while (true) {
+            System.out.println("Enter your Date of Return(dd/mm/yyyy) : ");
+            date = scan.nextLine();
+            try {
+                endDate = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+                lEndDate = LocalDate.ofInstant(endDate.toInstant(), ZoneId.systemDefault());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Start date: " + startDate);
+            System.out.println("Return date: " + endDate);
+            long diff = endDate.getTime() - startDate.getTime();
+            days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS) + 1;
+            if (diff < 0) {
+                System.out.println("Invalid return date!");
+            } else {
+                break;
+            }
         }
-        System.out.println("Start date: " + startDate + lStartDate);
-        System.out.println("Return date: " + endDate + lEndDate);
-        long diff = endDate.getTime() - startDate.getTime();
-        days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS) + 1;
-        if(diff < 0){
-            System.out.println("Invalid return date!");
-        } else {
-            System.out.println ("Days: " + days);
-            System.out.println();
-        }
-//        Writing everything to a file now
+        System.out.println("Days: " + days);
+        System.out.println();
+//            Writing everything to a file now
         writeInfo();
     }
+
     private void writeInfo() {
 //        Writing vehicle info into a file for further processing in the format
 //        registration number,  make, startDate, endDate, driverIdentifictionNumber
 
-        String tempRegNumber ;
+        String tempRegNumber;
         String tempMake;
         Random rnd = new Random();
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         int stateCode = 100;
-        while (stateCode > 54){
-            stateCode = Integer.parseInt(rnd.nextInt(10) +  rnd.nextInt(10) + "");
+        while (stateCode > 54) {
+            stateCode = Integer.parseInt(rnd.nextInt(10) + rnd.nextInt(10) + "");
         }
-        tempRegNumber = "KA-" + stateCode + " " +  (char) (rnd.nextInt(26) + 'A') + (char) (rnd.nextInt(26) + 'A') + "-" + rnd.nextInt(10) + rnd.nextInt(10) +rnd.nextInt(10) +rnd.nextInt(10) ;
+        tempRegNumber = "KA-" + stateCode + " " + (char) (rnd.nextInt(26) + 'A') + (char) (rnd.nextInt(26) + 'A') + "-" + rnd.nextInt(10) + rnd.nextInt(10) + rnd.nextInt(10) + rnd.nextInt(10);
         tempMake = rnd.nextInt(15) + 2005 + "";
         System.out.println("Registration number " + tempRegNumber + "\nMake : " + tempMake);
 
@@ -113,18 +118,23 @@ class Vehicle {
         System.out.println("Number of Days " + actualDays.getDays());
     }
 }
-class Luxury extends Vehicle{
+
+class Luxury extends Vehicle {
 
 }
-class Small_car extends Vehicle{
+
+class Small_car extends Vehicle {
     boolean ac;
 }
-class Family_car extends Vehicle{
+
+class Family_car extends Vehicle {
     boolean ac;
 }
-class Small_Van extends Vehicle{
+
+class Small_Van extends Vehicle {
     boolean ac;
 }
-class Large_Van extends Vehicle{
+
+class Large_Van extends Vehicle {
     boolean ac;
 }
