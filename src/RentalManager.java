2cd4b8997e3de1fba421d22268620bdb8cf7e589
akashrajkr <aakashrajkr@gmail.com>
2019-09-11 15:41:49 +0530

    Put the Home options in an infinite loop.

--- src/RentalManager.java
+++ src/RentalManager.java
@@ -10,38 +10,45 @@ import java.util.Scanner;
 
 class RentalManager{
     public static void main(String[] args) {
-        System.out.println("1.Rent a vehicle.\n2.List all the vehicles out for rent.\n3. Return a vehicle.\n4.Exit");
-        Scanner scan = new Scanner(System.in);
-        int ch = Integer.parseInt(scan.nextLine());
-        if(ch == 4){
-            System.out.println("exiting...");
-            exit(0);
-        }
-        else if(ch==1) {
-            rentVehicle();
-        }
-        else if(ch == 2){
-            try (BufferedReader vehicleDatabase = new BufferedReader(new FileReader("drivers.txt"))) {
-                vehicleDatabase.readLine();
-                System.out.println("List of vehicles out for rent: ");
-                String record;
-                if((record = vehicleDatabase.readLine()) == null){
-                    System.out.println("No vehicles out for rent!");
-                }
 
-                do {
-                    System.out.println(record.split(",")[0]);
-                } while (((record = vehicleDatabase.readLine()) != null));
-            } catch (FileNotFoundException e){
-                e.printStackTrace();
-                System.out.println("file not found! exiting...");
+        while(true) {
+            System.out.println("1.Rent a vehicle.\n2.List all the vehicles out for rent.\n3. Return a vehicle.\n4.Exit");
+            Scanner scan = new Scanner(System.in);
+            int ch = Integer.parseInt(scan.nextLine());
+            if(ch == 4){
+                System.out.println("exiting...");
                 exit(0);
-            } catch (IOException e) {
-
-                e.printStackTrace();
             }
-        } else if(ch == 3) {
-            returnVehicle();
+            else if(ch==1) {
+                rentVehicle();
+            }
+            else if(ch == 2){
+                try (BufferedReader vehicleDatabase = new BufferedReader(new FileReader("drivers.txt"))) {
+                    vehicleDatabase.readLine();
+                    System.out.println("List of vehicles out for rent: ");
+                    String record;
+                    if((record = vehicleDatabase.readLine()) == null){
+                        System.out.println("No vehicles out for rent!");
+                    }
+
+                    do {
+                        System.out.println(record.split(",")[0]);
+                    } while (((record = vehicleDatabase.readLine()) != null));
+                } catch (FileNotFoundException e){
+                    e.printStackTrace();
+                    System.out.println("file not found! exiting...");
+                    exit(0);
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+                }
+            } else if(ch == 3) {
+                returnVehicle();
+            } else if(ch == 4){
+                break;
+            } else {
+                System.out.println("Wrong choice...Enter again.");
+            }
         }
     }
 
